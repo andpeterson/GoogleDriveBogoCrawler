dcb7c6393e4f3f7004dc399094fb686d21000e76
Andrew Peterson <andrew@andpeterson.com>
2019-04-27 12:22:26 -0700

    Added Crawler code and first 500 error code

---
+++ crawler.py
@@ -0,0 +1,40 @@
+import requests
+import random
+import time
+seq = "1234567890-_qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+
+def main():
+    while(True):
+        string = randomString(33)
+        URL = "https://drive.google.com/file/d/" + string + "/view?usp=sharing"
+        res = requests.get(url = URL)
+        status = res.status_code
+        if status == 200:
+            file = open("200.txt", "a+")
+            file.write(URL + "\n")
+            file.close()
+            print(URL)
+        elif status == 500:
+            file = open("500.txt", "a+")
+            file.write(URL + "\n")
+            file.close()
+            print(str(status) + ": " + string)
+        elif status == 404:
+            print(str(status) + ": " + string)
+        else:
+            file = open("other.txt", "a+")
+            file.write(URL + "\n")
+            file.close()
+            print(str(status) + ": " + string)
+        #time.sleep(0.001)
+
+
+def randomString(len):
+    string = ""
+    for i in range(len):
+        string += (random.choice(seq))
+    return string
+
+
+main()
+
